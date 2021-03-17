cd9e97c74f40dd33fb3f3d7d8d8d5a949676f28c
JadeXTGitHub <78270947+JadeXTGitHub@users.noreply.github.com>
2021-03-17 15:34:57 -0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset='utf-8'>
+    <title>Lab 8</title>
+    <meta name='viewport' content='width=device-width, initial-scale=1'>
+    <link rel='stylesheet' type='text/css' media='screen' href='default.css'>
+ </head>
+<body>
+
+    <H1> COVID Vaccine Registrations</H1>
+    <table class="Table">
+        <thead>
+        <tr>
+        <th>County</th>
+        <th>Provider Name</th>
+        <th>Registration Website</th>
+        </tr>
+        </thead>
+        <tfoot>
+        <tr>
+        <td colspan="3">
+               </td>
+        </tr>
+        </tfoot>
+        <tbody>
+        <tr>
+        <td><img src="star.jfif" alt="Star" width="20" height="20">Dallas</td>
+        <td>Dallas County Health and Human Services</td>
+        <td><a href="https://www.dallascounty.org/covid-19/covid-19-vaccination.php">https://www.dallascounty.org/covid-19/covid-19-vaccination.php</a></td>
+        </tr>
+        <tr>
+        <td>Denton</td>
+        <td>Denton County Public Health</td>
+        <td><a href="http://www.dentoncounty.gov/vaccine">http://www.dentoncounty.gov/vaccine</td></a>
+        </tr>
+        <tr>
+        <td>Tarrant</td>
+        <td>Tarrant County Public Health District</td>
+        <td><a href="http://www.tarrantcounty.com/COVIDshot">http://www.tarrantcounty.com/COVIDshot</a></td>
+        </tr>
+        <tr>
+        <td>Collin</td>
+        <td>Collin County Healthcare Services</td>
+        <td> <a href= "https://www.collincountytx.gov/healthcare_services/Pages/COVID19vaccines.aspx">https://www.collincountytx.gov/healthcare_services/Pages/COVID19vaccines.aspx</a></td>
+        </tr>
+        <tr>
+        <td>Harris</td>
+        <td>Harris County Public Health</td>
+        <td><a href= "https://vacstrac.hctx.net/">https://vacstrac.hctx.net/</a></td>
+        </tr>
+        </tbody>
+        </table> 
+</body>
+</html>
